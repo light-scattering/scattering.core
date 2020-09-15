a1d58b83b7365aca1d1f1a4f15b8fba9954cb97a
Krzysztof Skorupski <krzysztof.skorupski@scattering.eu>
2020-09-16 00:24:14 +0200

    update

--- src/main/java/eu/scattering/core/design/Factory.java
+++ src/main/java/eu/scattering/core/design/Factory.java
@@ -1,8 +1,7 @@
 package eu.scattering.core.design;
 
-import eu.scattering.core.design.development.DevelopmentFactory;
 import eu.scattering.core.design.main.MainFactory;
 import eu.scattering.core.design.support.SupportFactory;
 
-public interface Factory extends MainFactory, SupportFactory, DevelopmentFactory {
+public interface Factory extends MainFactory, SupportFactory {
 }
