b67abd56ca270f7feb679a5a6b8cab5cac653893
Krzysztof Skorupski <k.skorupski@testify.no>
2025-02-17 17:55:03 +0100

    spring dependency removed

--- eu.scattering.core.impl/src/main/java/module-info.java
+++ eu.scattering.core.impl/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module eu.scattering.core.impl.def {
     requires transitive eu.scattering.core.design;
     requires org.json;
     requires java.annotation;
-    requires spring.context;
-    requires spring.beans;
     requires eu.scattering.core.transfer;
     exports eu.scattering.core.impl;
     opens eu.scattering.core.impl;
--- src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module eu.scattering.local {
-    requires spring.context;
-    requires eu.scattering.core.design;
-    requires eu.scattering.core.impl.def;
-}
