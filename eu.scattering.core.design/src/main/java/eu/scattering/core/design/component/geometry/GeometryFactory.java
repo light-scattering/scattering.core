90ec61ecb44e6999d531b1c6c74f4306aefb8716
Krzysztof Skorupski <k.skorupski@testify.no>
2025-03-04 12:52:39 +0100

    FCache update

--- eu.scattering.core.design/src/main/java/eu/scattering/core/design/component/geometry/GeometryFactory.java
+++ eu.scattering.core.design/src/main/java/eu/scattering/core/design/component/geometry/GeometryFactory.java
@@ -1,8 +1,6 @@
 package eu.scattering.core.design.component.geometry;
 
-import eu.scattering.core.design.component.geometry.base.BaseFactory;
-import eu.scattering.core.design.component.geometry.construct.ConstructFactory;
 import eu.scattering.core.design.component.geometry.shape.ShapeFactory;
 
-public interface GeometryFactory extends BaseFactory, ConstructFactory, ShapeFactory {
+public interface GeometryFactory extends ShapeFactory {
 }
--- eu.scattering.core.transfer/src/main/java/eu/scattering/core/transfer/container/buffer/FCache/FCache.java
+++ eu.scattering.core.transfer/src/main/java/eu/scattering/core/transfer/container/buffer/FCache/FCache.java
@@ -1,8 +1,63 @@
 package eu.scattering.core.transfer.container.buffer.FCache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FCache {
     private static final String JSON_MAIN = "cache";
     private static final String JSON_VAL = "dump";
 
+    private final Map<Long, Map<String, Object>> cache;
 
+    private FCache() {
+
+        this.cache = new HashMap<>();
+    }
+
+    public <T> T get(String key, Class<T> type) {
+
+        long threadId = Thread.currentThread().getId();
+
+        if (!cache.containsKey(threadId)) {
+            throw new IllegalArgumentException("The thread is not registered");
+        }
+
+        Map<String, Object> data = cache.get(threadId);
+
+        if (!data.containsKey(key)) {
+            throw new IllegalArgumentException("The value does not exist");
+        }
+
+        return type.cast(data.get(key));
+    }
+
+    public <T> T get(String key, Class<T> type, T element) {
+
+        long threadId = Thread.currentThread().getId();
+
+        if (!cache.containsKey(threadId)) {
+            cache.put(threadId, new HashMap<>());
+        }
+
+        Map<String, Object> data = cache.get(threadId);
+
+        if (!data.containsKey(key)) {
+            data.put(key, element);
+        }
+
+        return type.cast(data.get(key));
+    }
+
+    public void set(String key, Object element) {
+
+        long threadId = Thread.currentThread().getId();
+
+        if (!cache.containsKey(threadId)) {
+            cache.put(threadId, new HashMap<>());
+        }
+
+        Map<String, Object> data = cache.get(threadId);
+
+        data.put(key, element);
+    }
 }
