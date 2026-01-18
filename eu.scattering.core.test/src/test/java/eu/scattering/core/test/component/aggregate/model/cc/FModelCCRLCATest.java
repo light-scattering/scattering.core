e71645aab740e5bfa5c2278ba1fdee0d76d0fcea
Krzysztof Skorupski <k.skorupski@testify.no>
2026-01-18 23:38:19 +0100

    Validated CC RLCA method

--- eu.scattering.core.test/src/test/java/eu/scattering/core/test/component/aggregate/model/cc/FModelCCRLCATest.java
+++ eu.scattering.core.test/src/test/java/eu/scattering/core/test/component/aggregate/model/cc/FModelCCRLCATest.java
@@ -22,6 +22,47 @@ public class FModelCCRLCATest {
 
     @Disabled
     @Nested
+    @Tag("Predefined")
+    @DisplayName("Aggregation 3D - Predefined")
+    class AggregationPredefinedTest {
+
+        @Test
+        @DisplayName("Aggregation 3D - Visual")
+        void results3DA() {
+            int size = 3000;
+
+            FAggregate fAggregate = factory.getFAggregateContext().base().monodisperse(size, 1);
+            FModel fModel = factory.getFModelContext().cc().rlca(fAggregate);
+
+            fModel.build();
+
+            String json = fAggregate.toJSON().toString();
+            String model = factory.getExportAspect().getFAggregateContext().toNGSolve(fAggregate);
+
+            assertTrue(json.length() > 0);
+            assertTrue(model.length() > 0);
+        }
+
+        @Test
+        @DisplayName("Aggregation 2D - Visual")
+        void results2DA() {
+            int size = 3000;
+
+            FAggregate fAggregate = factory.getFAggregateContext().base().monodisperse(size, 1);
+            FModel fModel = factory.getFModelContext().cc().rlca(Dimension.D2, fAggregate);
+
+            fModel.build();
+
+            String json = fAggregate.toJSON().toString();
+            String model = factory.getExportAspect().getFAggregateContext().toNGSolve(fAggregate);
+
+            assertTrue(json.length() > 0);
+            assertTrue(model.length() > 0);
+        }
+    }
+
+    @Disabled
+    @Nested
     @Tag("Heavy")
     @DisplayName("Aggregation 3D - Heavy")
     class AggregationHeavyTest {
