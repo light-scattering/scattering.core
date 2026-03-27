666e2c8018e043af37160da86e7ebfd219e84823
Krzysztof Skorupski <k.skorupski@testify.no>
2026-03-27 18:15:41 +0100

    Extracted BC method

--- eu.scattering.core.lib/src/main/java/eu/scattering/core/impl/component/aggregate/FAggregateModuleFractalDimensionDef.java
+++ eu.scattering.core.lib/src/main/java/eu/scattering/core/impl/component/aggregate/FAggregateModuleFractalDimensionDef.java
@@ -11,23 +11,23 @@ import eu.scattering.core.design.statistics.construct.plot.FPlot;
 import eu.scattering.core.design.statistics.construct.plot.FPlotMeta;
 import eu.scattering.core.design.storage.transfer.polynomial.variant.FPoly;
 import eu.scattering.core.design.storage.transfer.position.p1.variant.FPos3D;
-import eu.scattering.core.design.storage.transfer.position.p2.variant.FPairPos3D;
 import eu.scattering.core.design.utility.type.variant.FractalDimension;
-import eu.scattering.core.design.utility.type.option.Length;
 import eu.scattering.core.design.utility.type.method.RadiusOfGyration;
 
 import java.util.*;
-
-import static eu.scattering.core.impl.ScatConfigDef.EPSILON;
 
 public class FAggregateModuleFractalDimensionDef {
     private final ScatFactory factory;
     private final FAggregate aggregate;
 
+    private final FAggregateModuleFractalDimensionBCDef bc;
+
     protected FAggregateModuleFractalDimensionDef(ScatFactory factory, FAggregate aggregate) {
 
         this.factory = factory;
         this.aggregate = aggregate;
+
+        this.bc = new FAggregateModuleFractalDimensionBCDef(factory, aggregate);
     }
 
     // -------------------------------------------------------------------------------------------------
@@ -113,192 +113,7 @@ public class FAggregateModuleFractalDimensionDef {
     // -------------------------------------------------------------------------------------------------
 
     protected FPlot getBoxCoverageFunction(double stepFactor, int offset, boolean start, boolean shift,  boolean pca) {
-
-        if (stepFactor <= 1) {
-            throw new IllegalArgumentException("The factor value must be greater then one");
-        }
-
-        if (offset < 1) {
-            throw new IllegalArgumentException("The offset value must be greater then zero");
-        }
-
-        FPlot results = this.factory.getFPlot();
-
-        FAggregate reference = this.aggregate.copy(false);
-
-        if (pca) {
-            reference.pca();
-        }
-
-        if (shift) {
-            reference.shiftBoundaryToZero();
-        }
-
-        FAggregate replica = reference.copy(false);
-
-        double radius = reference.getFStatParticleRadius().mean();
-        double scaleFactor = 1 / stepFactor;
-
-        List<FPos3D> offsets = getOffsetValues(offset);
-
-        double cutoffInner = radius * 2;
-        double cutoffOuter = reference.getLength(Length.MAX);
-
-        if (start) {
-            results.add(cutoffOuter, 1);
-        }
-
-        double box = cutoffOuter * scaleFactor;
-        while (box >= cutoffInner) {
-            getBoxCoverageFunctionStep(reference, replica, offsets, results, box);
-
-            box *= scaleFactor;
-        }
-
-        return results;
-    }
-
-    protected FPlot getBoxCoverageFunctionBruteForce() {
-        FPlot results = this.factory.getFPlot();
-
-        FAggregate reference = this.aggregate.copy(false);
-
-        double radius = reference.getFStatParticleRadius().mean();
-        double scaleFactor = 0.5;
-
-        double cutoffInner = radius * 2;
-        double cutoffOuter = reference.getLength(Length.MAX);
-
-        double box = cutoffOuter * scaleFactor;
-        while (box >= cutoffInner) {
-            getBoxCoverageFunctionBruteForceStep(reference, results, box);
-
-            box = box * scaleFactor;
-        }
-
-        return results;
-    }
-
-    private void getBoxCoverageFunctionStep(FAggregate reference, FAggregate replica, List<FPos3D> offsets, FPlot results, double box) {
-        FSphereHelper sphereHelper = this.factory.getFSphereHelper();
-        int sumMin = Integer.MAX_VALUE;
-        double scale = 1 / box;
-
-        for (FPos3D offset : offsets) {
-
-            for (int i = 0 ; i < reference.size() ; i++) {
-                replica.getRefParticles().asList().get(i).setCenter(reference.getRefParticles().asList().get(i).getRefCenter());
-                replica.getRefParticles().asList().get(i).setRadius(reference.getRefParticles().asList().get(i).getRadius());
-            }
-
-            Queue<Shape> particles = new LinkedList<>(replica.getRefParticles().asList());
-            particles.forEach(e -> e.scalePosition(scale).scaleSize(scale));
-            particles.forEach(e -> e.translate(offset));
-
-            List<Shape> neighbours = new ArrayList<>(particles.size());
-
-            int sum = 0;
-            while (!particles.isEmpty()) {
-                Shape particle = particles.poll();
-
-                neighbours.clear();
-
-                particles.forEach(e -> {
-                    if (e.getDistCenterP2(particle) < Math.pow(e.getRadius() + particle.getRadius() + 2, 2)) {
-                        neighbours.add(e);
-                    }
-                });
-
-                int minX = (int) Math.floor(particle.getCenterX() - particle.getRadius() + EPSILON);
-                int minY = (int) Math.floor(particle.getCenterY() - particle.getRadius() + EPSILON);
-                int minZ = (int) Math.floor(particle.getCenterZ() - particle.getRadius() + EPSILON);
-
-                int maxX = (int) Math.ceil(particle.getCenterX() + particle.getRadius() - EPSILON);
-                int maxY = (int) Math.ceil(particle.getCenterY() + particle.getRadius() - EPSILON);
-                int maxZ = (int) Math.ceil(particle.getCenterZ() + particle.getRadius() - EPSILON);
-
-                for (int x = minX; x < maxX; x++) {
-                    for (int y = minY; y < maxY; y++) {
-
-                        next:
-                        for (int z = minZ; z < maxZ; z++) {
-                            if (sphereHelper.intersectsCube(particle, x, y, z, 1)) {
-                                for (Shape neighbour : neighbours) {
-                                    if (sphereHelper.intersectsCube(neighbour, x, y, z, 1)) {
-                                        continue next;
-                                    }
-                                }
-
-                                sum++;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (sum < sumMin) {
-                sumMin = sum;
-            }
-        }
-
-        results.add(box, sumMin);
-    }
-
-    private void getBoxCoverageFunctionBruteForceStep(FAggregate reference, FPlot data, double step) {
-        FSphereHelper sphereHelper = this.factory.getFSphereHelper();
-        FPairPos3D boundary = reference.getBoundary();
-
-        double minX = boundary.getPosA().getD0();
-        double minY = boundary.getPosA().getD1();
-        double minZ = boundary.getPosA().getD2();
-
-        double widthX = boundary.getPosB().getD0() - minX;
-        double widthY = boundary.getPosB().getD1() - minY;
-        double widthZ = boundary.getPosB().getD2() - minZ;
-
-        int sizeX = (int) Math.ceil(widthX / step);
-        int sizeY = (int) Math.ceil(widthY / step);
-        int sizeZ = (int) Math.ceil(widthZ / step);
-
-        int sum = 0;
-        for (int i = 0; i < sizeX; i++) {
-            double x = minX + (i * step);
-
-            for (int j = 0; j < sizeY; j++) {
-                double y = minY + ( j * step);
-
-                next:
-                for (int k = 0; k < sizeZ; k++) {
-                    double z = minZ + (k * step);
-
-                    for (Shape particle : reference) {
-                        if (sphereHelper.intersectsCube(particle, x, y, z, step)) {
-                            sum++;
-
-                            continue next;
-                        }
-                    }
-                }
-            }
-        }
-
-        data.add(step, sum);
-    }
-
-    private List<FPos3D> getOffsetValues(int count) {
-        List<FPos3D> results = new ArrayList<>(count * count * count);
-
-        double shift = (double) 1 / count;
-
-        for (int i = 0 ; i < count ; i++) {
-            for (int j = 0 ; j < count ; j++) {
-                for (int k = 0 ; k < count ; k++) {
-                    results.add(this.factory.getFPos3D(i * shift, j * shift, k * shift));
-                }
-            }
-        }
-
-        return results;
+        return this.bc.getResultsOptimized(stepFactor, offset, start, shift, pca);
     }
 
     // -------------------------------------------------------------------------------------------------
@@ -432,16 +247,16 @@ public class FAggregateModuleFractalDimensionDef {
     protected double getFractalDimension(FractalDimension type) {
 
         return switch (type) {
-            case BC_REFERENCE -> getBoxCountingAnalyze(
-                    getBoxCoverageFunctionBruteForce(),
+            case BC_REFERENCE -> this.bc.analyze(
+                    this.bc.getResultsBruteForce(),
                     1
             );
-            case BC_SIMPLIFIED -> getBoxCountingAnalyze(
-                    getBoxCoverageFunction(2, 1, false, true, false),
+            case BC_SIMPLIFIED -> this.bc.analyze(
+                    this.bc.getResultsOptimized(2, 1, false, true, false),
                     1
             );
-            case BC_OPTIMIZED -> getBoxCountingAnalyze(
-                    getBoxCoverageFunction(2, 3, false, false, false),
+            case BC_OPTIMIZED -> this.bc.analyze(
+                    this.bc.getResultsOptimized(2, 3, false, false, false),
                     0.9
             );
             case CORRELATION -> getDensityCorrelationAnalyze(
@@ -470,7 +285,7 @@ public class FAggregateModuleFractalDimensionDef {
 
     protected double getFractalDimensionBoxCounting(double window, double stepFactor, int offset, boolean start, boolean shift, boolean pca) {
 
-        return getBoxCountingAnalyze(getBoxCoverageFunction(stepFactor, offset, start, shift, pca), window);
+        return this.bc.analyze(this.bc.getResultsOptimized(stepFactor, offset, start, shift, pca), window);
     }
 
     protected double getFractalDimensionDensityCorrelation(double window, RadiusOfGyration method, double stepFactor, boolean rangeLimit) {
@@ -506,31 +321,6 @@ public class FAggregateModuleFractalDimensionDef {
 
         data.mutateY((x, y) -> Math.log(y));
         data.mutateX((x, y) -> Math.log(x));
-
-        data.filter((x, y) -> y > 0);
-
-        FPoly regression = window == 1 ? data.reg().poly(1) : data.reg().fitSlope((int) (data.size() * window));
-
-        FPlot fit = data.copy();
-        fit.setY(regression);
-
-        FPlotMeta plotConfig = factory.getFPlotMeta()
-                .setAnnotation("Test data");
-
-        String plot = factory.getSaveAspect().getStatisticsContext()
-                .toPythonPlotly(plotConfig, data, fit);
-
-        return regression.at(1);
-    }
-
-    private double getBoxCountingAnalyze(FPlot data, double window) {
-
-        if (window <= 0 || window > 1) {
-            throw new IllegalArgumentException("The window must be in range 0-1");
-        }
-
-        data.mutateY((x, y) -> Math.log(y));
-        data.mutateX((x, y) -> Math.log(1 / x));
 
         data.filter((x, y) -> y > 0);
 
