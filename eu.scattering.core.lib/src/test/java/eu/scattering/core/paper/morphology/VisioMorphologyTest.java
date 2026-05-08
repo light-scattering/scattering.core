363da5d028408cd5b2a7f962214c6416cc758751
Krzysztof Skorupski <k.skorupski@testify.no>
2026-05-08 23:34:55 +0200

    Added the morphology test

---
+++ eu.scattering.core.lib/src/test/java/eu/scattering/core/paper/morphology/VisioMorphologyTest.java
@@ -0,0 +1,79 @@
+package eu.scattering.core.paper.morphology;
+
+import eu.scattering.core.design.utility.type.preset.ExPovRay;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static eu.scattering.core.test.Config.factory;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Disabled
+@DisplayName("Paper - Morphology")
+public class VisioMorphologyTest {
+
+    @Test
+    @Tag("Visual")
+    @DisplayName("Morphology - Df = 1.4")
+    void df14() {
+        int size = 10000;
+        double r = 1;
+        double df = 1.4;
+        double kf = 1.5;
+
+        var fAggregate = factory.getFAggregateContext().base().monodisperse(size, r);
+
+        var fModel = factory.getFModelContext().cc().tunable(fAggregate, df, kf);
+        fModel.setEarlyStageCorrection(true);
+
+        fModel.build();
+
+        String model = factory.getSaveAspect().getComponentContext().toPovRay(fAggregate, ExPovRay.BOUNDARY);
+
+        assertFalse(model.isEmpty());
+    }
+
+    @Test
+    @Tag("Visual")
+    @DisplayName("Morphology - Df = 1.8")
+    void df18() {
+        int size = 10000;
+        double r = 1;
+        double df = 1.8;
+        double kf = 1.3;
+
+        var fAggregate = factory.getFAggregateContext().base().monodisperse(size, r);
+
+        var fModel = factory.getFModelContext().cc().tunable(fAggregate, df, kf);
+        fModel.setEarlyStageCorrection(true);
+
+        fModel.build();
+
+        String model = factory.getSaveAspect().getComponentContext().toPovRay(fAggregate, ExPovRay.BOUNDARY);
+
+        assertFalse(model.isEmpty());
+    }
+
+    @Test
+    @Tag("Visual")
+    @DisplayName("Morphology - Df = 2.2")
+    void df22() {
+        int size = 10000;
+        double r = 1;
+        double df = 2.2;
+        double kf = 0.8;
+
+        var fAggregate = factory.getFAggregateContext().base().monodisperse(size, r);
+
+        var fModel = factory.getFModelContext().cc().tunable(fAggregate, df, kf);
+        fModel.setEarlyStageCorrection(true);
+
+        fModel.build();
+
+        String model = factory.getSaveAspect().getComponentContext().toPovRay(fAggregate, ExPovRay.BOUNDARY);
+
+        assertFalse(model.isEmpty());
+    }
+}
