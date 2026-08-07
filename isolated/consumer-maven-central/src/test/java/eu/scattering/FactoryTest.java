c4e906bcb00ef06b8ca0174975736a11f3f70912
Krzysztof Skorupski <k.skorupski@testify.no>
2026-08-07 22:09:15 +0200

    build(gradle): setup local and production publishing pipeline

---
+++ isolated/consumer-maven-central/src/test/java/eu/scattering/FactoryTest.java
@@ -0,0 +1,23 @@
+package eu.scattering;
+
+import eu.scattering.core.design.ScatterFactory;
+import eu.scattering.core.design.component.aggregate.FAggregate;
+import eu.scattering.core.impl.ScatterCore;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Consumer test")
+public class FactoryTest {
+
+    @Test
+    @DisplayName("Create factory")
+    void getFactory() {
+        ScatterFactory factory = ScatterCore.createFactory();
+
+        FAggregate fAggregate = factory.getFAggregateContext().geometry().d3(3, 4, 5);
+
+        assertEquals(60, fAggregate.size());
+    }
+}
